6e826b94a7d2c057242c31c955f3a84135ae8289
yutaro.hosokawa <utaro.89@gmail.com>
2019-12-07 02:49:19 +0000

    login_fixed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   def index
     if logged_in?
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
