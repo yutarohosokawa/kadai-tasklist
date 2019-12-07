a20859b6147425cc6f8fb4c65ac0115a00e8f8a5
yutaro.hosokawa <utaro.89@gmail.com>
2019-12-07 08:00:43 +0000

    error_message_fixed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,11 +17,11 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'メッセージを投稿しました。'
+      flash[:success] = 'タスクを投稿しました。'
       redirect_to root_url
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
       render :new
     end
   end
@@ -34,10 +34,10 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
+      flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'タスクは更新されませんでした'
       render :edit
     end
   end
