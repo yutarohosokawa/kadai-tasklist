453497881b5632077074543a186fc2e160da7b72
yutaro.hosokawa <utaro.89@gmail.com>
2019-12-22 09:20:50 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   def index
     if logged_in?
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
@@ -55,5 +56,13 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+  @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+
 end
 
